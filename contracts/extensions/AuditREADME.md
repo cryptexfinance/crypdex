5c27aa280bb00ca87580dd1018c48213f0ad3389
voith <voithjm1@gmail.com>
2024-09-10 04:37:49 +0530

    remove exchange.call external call

--- contracts/extensions/AuditREADME.md
+++ contracts/extensions/AuditREADME.md
@@ -21,8 +21,7 @@ This function is used to purchase components and issue a Set Token. It interacts
 - `setTokenQuantity`: The quantity of the SetToken to issue.
 - `quoteAsset`: The instance of the IERC20 token used for buying the underlying components.
 - `totalQuoteAmount`: The maximum amount the user pays for buying the underlying components.
-- `exchanges`: An array of addresses used to buy each component.
-- `exchangePayloads`: Payloads targeted towards each exchange for buying the corresponding component.
+- `exchangeParams`: An array of containing data to buy each component.
 
 #### Example Usage
 
@@ -40,18 +39,8 @@ This function allows for the redemption of a Set Token and the exchange of its c
 - `setTokenQuantity`: Quantity of the SetToken to redeem. 
 - `quoteAsset`: Instance of the IERC20 token received after selling the underlying components. 
 - `minQuoteAmount`: The minimum `quoteAsset` amount the user expects to receive after selling the underlying components. 
-- `exchanges`: An array of addresses used to sell each component. 
-- `exchangePayloads`: Payloads targeted towards each exchange for selling the corresponding component.
+- `exchangeParams`: An array of containing data to sell each component. 
+
 
 #### Example Usage
 Please refer to this [test](https://github.com/cryptexfinance/crypdex/blob/d0de0f9a85bc632fd88fd42c2028705b089836fe/test/unit/ForkTestTokenExchangeSetIssuer.t.sol#L335-L370).
-
-### Audit Scope
-
-For the purposes of this audit, only the following exchanges are whitelisted for usage on mainnet:
-
-- **Paraswap v6**
-  - Address: `0x6A000F20005980200259B80c5102003040001068`
-
-- **Uniswap Router**
-  - Address: `0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D`
---
+++ contracts/interfaces/external/IParaswapV6.sol
@@ -0,0 +1,149 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.6.10;
+pragma experimental ABIEncoderV2;
+
+struct GenericData {
+    address srcToken;
+    address destToken;
+    uint256 fromAmount;
+    uint256 toAmount;
+    uint256 quotedAmount;
+    bytes32 metadata;
+    address payable beneficiary;
+}
+
+struct BalancerV2Data {
+    uint256 fromAmount;
+    uint256 toAmount;
+    uint256 quotedAmount;
+    bytes32 metadata;
+    uint256 beneficiaryAndApproveFlag;
+}
+
+struct CurveV1Data {
+    uint256 curveData;
+    uint256 curveAssets;
+    address srcToken;
+    address destToken;
+    uint256 fromAmount;
+    uint256 toAmount;
+    uint256 quotedAmount;
+    bytes32 metadata;
+    address payable beneficiary;
+}
+
+struct CurveV2Data {
+    uint256 curveData;
+    uint256 i;
+    uint256 j;
+    address poolAddress;
+    address srcToken;
+    address destToken;
+    uint256 fromAmount;
+    uint256 toAmount;
+    uint256 quotedAmount;
+    bytes32 metadata;
+    address payable beneficiary;
+}
+
+struct UniswapV2Data {
+    address srcToken;
+    address destToken;
+    uint256 fromAmount;
+    uint256 toAmount;
+    uint256 quotedAmount;
+    bytes32 metadata;
+    address payable beneficiary;
+    bytes pools;
+}
+
+struct UniswapV3Data {
+    address srcToken;
+    address destToken;
+    uint256 fromAmount;
+    uint256 toAmount;
+    uint256 quotedAmount;
+    bytes32 metadata;
+    address payable beneficiary;
+    bytes pools;
+}
+
+interface IParaswapV6 {
+    function swapExactAmountIn(
+        address executor,
+        GenericData calldata swapData,
+        uint256 partnerAndFee,
+        bytes calldata permit,
+        bytes calldata executorData
+    ) external payable returns (uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
+
+    function swapExactAmountOut(
+        address executor,
+        GenericData calldata swapData,
+        uint256 partnerAndFee,
+        bytes calldata permit,
+        bytes calldata executorData
+    )
+        external
+        payable
+        returns (uint256 spentAmount, uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
+
+    function swapExactAmountInOnBalancerV2(
+        BalancerV2Data calldata balancerData,
+        uint256 partnerAndFee,
+        bytes calldata permit,
+        bytes calldata data
+    ) external payable returns (uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
+
+    function swapExactAmountOutOnBalancerV2(
+        BalancerV2Data calldata balancerData,
+        uint256 partnerAndFee,
+        bytes calldata permit,
+        bytes calldata data
+    )
+        external
+        payable
+        returns (uint256 spentAmount, uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
+
+    function swapExactAmountInOnCurveV1(
+        CurveV1Data calldata curveV1Data,
+        uint256 partnerAndFee,
+        bytes calldata permit
+    ) external payable returns (uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
+
+    function swapExactAmountInOnCurveV2(
+        CurveV2Data calldata curveV2Data,
+        uint256 partnerAndFee,
+        bytes calldata permit
+    ) external payable returns (uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
+
+    function swapExactAmountInOnUniswapV2(
+        UniswapV2Data calldata uniData,
+        uint256 partnerAndFee,
+        bytes calldata permit
+    ) external payable returns (uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
+
+    function swapExactAmountOutOnUniswapV2(
+        UniswapV2Data calldata uniData,
+        uint256 partnerAndFee,
+        bytes calldata permit
+    )
+        external
+        payable
+        returns (uint256 spentAmount, uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
+
+    function swapExactAmountInOnUniswapV3(
+        UniswapV3Data calldata uniData,
+        uint256 partnerAndFee,
+        bytes calldata permit
+    ) external payable returns (uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
+
+    function swapExactAmountOutOnUniswapV3(
+        UniswapV3Data calldata uniData,
+        uint256 partnerAndFee,
+        bytes calldata permit
+    )
+        external
+        payable
+        returns (uint256 spentAmount, uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
+}
